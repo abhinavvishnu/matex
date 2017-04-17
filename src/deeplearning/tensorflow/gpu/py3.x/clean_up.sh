505044cb94381bc3c592077ba7fad96da6e20f9e
Joseph Manzano <joseph.manzano@pnnl.gov>
2017-04-16 22:19:04 -0700

    Cosmetic improvements and addign the sync op

--- src/deeplearning/tensorflow/gpu/py3.x/clean_up.sh
+++ src/deeplearning/tensorflow/gpu/py3.x/clean_up.sh
@@ -2,6 +2,7 @@
 
 if [ -d $PWD/py_distro ]; then 
    deactivate
+   cd $PWD/user_ops ; make clean ; cd $PWD
    rm -rf $PWD/py_distro
    if [ -z "$OLD_PYTHONHOME" ]; then 
       echo "PYTHONHOME would be unset. Please correct if incorrect"
