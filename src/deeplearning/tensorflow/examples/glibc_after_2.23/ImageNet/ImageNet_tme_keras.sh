c7ef90faf05c43bf4dc81bb3fc6b3ecb97c636c5
Joseph Manzano <joseph.manzano@pnnl.gov>
2017-05-31 14:38:31 -0700

    Some bug fixes

--- src/deeplearning/tensorflow/examples/glibc_after_2.23/ImageNet/ImageNet_tme_keras.sh
+++ src/deeplearning/tensorflow/examples/glibc_after_2.23/ImageNet/ImageNet_tme_keras.sh
@@ -26,10 +26,10 @@ while getopts ":b:n:" opt; do
 done
 
 train_batch=`expr $train_batch / $nodes`
-echo "Using $nodes for computing with a batch size
+echo "Using $nodes for computing with a batch size $train_batch"
 
-    mpirun --map-by node -n $(nodes) --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch $(train_batch) --iterations 500 --network "AlexNet"
-    mpirun --map-by node -n $(nodes) --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch $(train_batch) --iterations 500 --network "InceptionV3"
-    mpirun --map-by node -n $(nodes) --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch $(train_batch) --iterations 500 --network "ResNet50"
-    mpirun --map-by node -n $(nodes) --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch $(train_batch) --iterations 500 --network "GoogLeNet"
+    mpirun --map-by node -n $nodes --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch $train_batch --iterations 500 --network "AlexNet"
+    mpirun --map-by node -n $nodes --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch $train_batch --iterations 500 --network "InceptionV3"
+    mpirun --map-by node -n $nodes --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch $train_batch --iterations 500 --network "ResNet50"
+    mpirun --map-by node -n $nodes --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch $train_batch --iterations 500 --network "GoogLeNet"
 
