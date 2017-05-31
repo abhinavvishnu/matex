0ff4e5f523868d810b44a0764102b4d3545dfc7e
Joseph Manzano <joseph.manzano@pnnl.gov>
2017-05-31 11:56:07 -0700

    More updates to the scripts

--- src/deeplearning/tensorflow/examples/glibc_after_2.23/ImageNet/ImageNet_tme_keras.sh
+++ src/deeplearning/tensorflow/examples/glibc_after_2.23/ImageNet/ImageNet_tme_keras.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
-#SBATCH -N 1
-#SBATCH -J ImageNet.Keras.1
-#SBATCH -o ImageNet.Keras.1.out.%j
-#SBATCH -e ImageNet.Keras.1.err.%j
+#SBATCH -J ImageNet.Keras
+#SBATCH -o ImageNet.Keras.out.%j
+#SBATCH -e ImageNet.Keras.err.%j
 
-    mpirun --map-by node -n 1  --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch 128 --iterations 500 --network "AlexNet"
-    mpirun --map-by node -n 1  --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch 128 --iterations 500 --network "InceptionV3"
-    mpirun --map-by node -n 1  --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch 128 --iterations 500 --network "ResNet50"
-    mpirun --map-by node -n 1  --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch 128 --iterations 500 --network "GoogLeNet"
+train_batch=128
+nodes=1
+
+while getopts ":b:n:" opt; do
+   case $opt in
+      b)
+         train_batch=$OPTARG
+         ;;
+      n)
+         nodes=$OPTARG
+         ;;
+      :)
+         echo "Option -$OPTARG requires an Argument"
+         exit 1
+         ;;
+      \?)
+         echo "Unknown option: $OPTARG"
+         exit 1
+         ;;
+   esac
+done
+
+train_batch=`expr $train_batch / $nodes`
+echo "Using $nodes for computing with a batch size
+
+    mpirun --map-by node -n $(nodes) --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch $(train_batch) --iterations 500 --network "AlexNet"
+    mpirun --map-by node -n $(nodes) --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch $(train_batch) --iterations 500 --network "InceptionV3"
+    mpirun --map-by node -n $(nodes) --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch $(train_batch) --iterations 500 --network "ResNet50"
+    mpirun --map-by node -n $(nodes) --mca opal_event_include poll $PYTHONHOME/bin/python $PWD/keras_timetest.py --train_batch $(train_batch) --iterations 500 --network "GoogLeNet"
 
